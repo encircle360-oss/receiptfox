3c4786e811ccd3b53b015b85981634f18a60315b
peter <ps@web-eifel.de>
2021-09-28 14:02:36 +0200

    added receipt state machine process controller

---
+++ src/main/java/com/encircle360/oss/receiptfox/dto/receipt/ReceiptEventDTO.java
@@ -0,0 +1,6 @@
+package com.encircle360.oss.receiptfox.dto.receipt;
+
+public enum ReceiptEventDTO {
+    SET_OPEN, SET_PAID, SET_CANCELED
+
+}
---
+++ src/main/java/com/encircle360/oss/receiptfox/mapping/ReceiptEventMapper.java
@@ -0,0 +1,16 @@
+package com.encircle360.oss.receiptfox.mapping;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.encircle360.oss.receiptfox.dto.receipt.ReceiptEventDTO;
+import com.encircle360.oss.receiptfox.model.receipt.ReceiptEvent;
+
+@Mapper
+public interface ReceiptEventMapper {
+
+    ReceiptEventMapper INSTANCE = Mappers.getMapper(ReceiptEventMapper.class);
+
+    ReceiptEvent fromDto(ReceiptEventDTO receiptEventDTO);
+
+}
