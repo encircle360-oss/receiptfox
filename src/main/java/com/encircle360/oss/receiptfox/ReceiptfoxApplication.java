5a508fea683d51f6c10bc90ad915c27a7e8aeb58
peter <ps@web-eifel.de>
2021-10-05 14:42:25 +0200

    removed freemarker totally, moved async executor ThreadConfiguration

--- src/main/java/com/encircle360/oss/receiptfox/ReceiptfoxApplication.java
+++ src/main/java/com/encircle360/oss/receiptfox/ReceiptfoxApplication.java
@@ -1,14 +1,10 @@
 package com.encircle360.oss.receiptfox;
 
-import java.util.concurrent.Executor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @EnableFeignClients
@@ -20,8 +16,4 @@ public class ReceiptfoxApplication {
         SpringApplication.run(ReceiptfoxApplication.class, args);
     }
 
-    @Bean
-    public Executor asyncExecutor() {
-        return new ThreadPoolTaskExecutor();
-    }
 }
